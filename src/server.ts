879fe11f8ba62e7767ce27d87aeed48fc7d4b606
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-11-26 22:29:46 +0600

    Recreate.Enb file

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,20 @@ const port = 5000;
 
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    try {
+        await mongoose.connect('mongodb+srv://phUniversity:fgUclelzryPQkPUm@cluster0.vcouptk.mongodb.net/phUniversity?retryWrites=true&w=majority&appName=Cluster0');
 
-    
+        
+
+    } catch (error) {
+
+    }
+
 }
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    console.log(process.env.DB_USER);
 })
 
 main()
