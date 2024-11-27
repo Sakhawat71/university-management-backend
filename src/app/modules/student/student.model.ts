972bf03583161ff0110325175ba2a1dd2619a25a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-11-27 09:46:56 +0600

    mongoose schema , model and zod validation for student

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, model } from "mongoose";
+import { IStudent } from "./student.interface";
+
+
+const UserNameSchema = new Schema({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const GuardianSchema = new Schema({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const LocalGuardianSchema = new Schema({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const StudentSchema = new Schema<IStudent>({
+  id: { type: String, required: true, unique: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  password: { type: String, required: true },
+  name: { type: UserNameSchema, required: true },
+  gender: { type: String, enum: ["male", "female", "other"], required: true },
+  dateOfBirth: { type: String },
+  email: { type: String, required: true, unique: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: { type: String, enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"] },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: { type: GuardianSchema, required: true },
+  localGuardian: { type: LocalGuardianSchema, required: true },
+  profileImg: { type: String },
+  isDeleted: { type: Boolean, default: false },
+}, {
+  timestamps: true,
+});
+
+export const StudentModel = model<IStudent>("Student", StudentSchema);
