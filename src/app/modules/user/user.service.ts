4973702b2ce7156ca5e141c0da8840339ebb1726
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-02 02:01:12 +0600

    user service -> create student

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,23 @@
 import config from "../../config";
 import { IStudent } from "../student/student.interface";
+import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 
 
-const createStudentIntoDb = async (password : string,newStudent: IStudent) => {
-    // return await StudentModel.create(newStudent);
+const createStudentIntoDb = async (password: string, studentData: IStudent) => {
 
-    if(!password){
-        password = config.default_pass as string;
+    const user: Partial<IUser> = {};
+    user.password = password || config.default_pass as string;
+    user.role = "student";
+    user.id = '20300100001';
+
+    const result = await UserModel.create(user);
+
+    if(Object.keys(result).length){
+        studentData.id = result.id;
+        studentData.user = result._id;
     }
-    return await UserModel.create(newStudent);
+    
 }
 
 
