e3d59832618f98a5f46f7a062c17050e87679348
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-04 03:05:13 +0600

    academic semester interface

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,24 @@
+type TMonths =
+    | 'January'
+    | 'February'
+    | 'March'
+    | 'April'
+    | 'May'
+    | 'June'
+    | 'July'
+    | 'August'
+    | 'September'
+    | 'October'
+    | 'November'
+    | 'December';
+
+type TName = "Autumn" | "Summer" | "Fall";
+type TCode =  "1" | "2" | "3";
+
+export interface IAcademicSemester {
+    name: TName;
+    code: TCode;
+    year: Date;
+    startMonth: TMonths;
+    endMonth: TMonths;
+};
