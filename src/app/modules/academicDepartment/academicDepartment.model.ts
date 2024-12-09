de395bb0ccb9de4b606a01f84832a6c2510b504a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-10 03:46:07 +0600

    Pre-save Middleware

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -17,7 +17,29 @@ const academicDepartmentSchema = new Schema<IAcademicDepartment>(
         timestamps: true
     }
 );
- 
+
+// Mongoose Middlewares -> Pre-save Middleware
+academicDepartmentSchema.pre('save', async function (next) {
+    const isDepartmentExist = await AcademicDepartmentModel.findOne({
+        name : this.name
+    });
+    if(isDepartmentExist){
+        throw new Error("This department is already exist!")
+    }
+    next()
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
+    const query = this.getQuery();
+    const isDepartmentExist = await AcademicDepartmentModel.findOne({
+        query
+    });
+    if(!isDepartmentExist){
+        throw new Error("This department dose not exist!")
+    }
+    next();
+});
+
 export const AcademicDepartmentModel = model<IAcademicDepartment>(
     'AcademicDepartment',
     academicDepartmentSchema
