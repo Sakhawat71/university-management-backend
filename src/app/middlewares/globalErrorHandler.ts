15eda892d56e87974c6e44f59d6fe03435f5d52e
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-12 14:31:52 +0600

    zod error Management

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,40 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ErrorRequestHandler} from "express";
-import { ZodError } from "zod";
+import { ErrorRequestHandler } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/error";
 
 const globalErrorHandler: ErrorRequestHandler = async (err, req, res, next): Promise<any> => {
-
+    //setting default values
     let statusCode = err.statusCodes || 500;
     let message = err.message || "Something went wrong !";
-
-    type TErrorSource = {
-        path: string | number;
-        message: string;
-    }[];
-
-    const errorSources : TErrorSource = [
+    let errorSources: TErrorSource = [
         {
-            path : '',
+            path: '',
             message: 'Something went wrong'
         },
     ];
 
-    if(err instanceof ZodError){
-        statusCode = 400;
-        message = 'this is zod error'
-    }
-    // else{
-    //     statusCode = 404;
-    //     message = 'i dont get it'
-    // }
 
+    // zod error Management
+    const handelZodError = (error : ZodError) => {
+        const errorSources : TErrorSource = error.issues.map((issue: ZodIssue) => {
+            return {
+                path : issue?.path[issue.path.length-1],
+                message : issue.message,
+            }
+        })
+        const statusCode = 400;
+        return {
+            statusCode,
+            message: 'zod validation error',
+            errorSources,
+        }
+    };
+
+    // instance of -> All kind of error
+    if (err instanceof ZodError) {
+        const simplifiedError = handelZodError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    }
+
+    // Ultimate return
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err,
         errorSources,
+        error: err,
     })
 }
 export default globalErrorHandler;
