c17e9556f587a812b2cb927697e9d8184f56822d
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-20 14:54:10 +0600

    declare global for user

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,18 +2,43 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import AppError from "../errors/appError";
 import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import config from "../config";
+
 
 const authValidation = () => {
     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
         const token = req.headers.authorization;
-        if(!token) {
+
+        // if the token sent from the client
+        if (!token) {
             throw new AppError(
                 StatusCodes.UNAUTHORIZED,
                 "Unauthorized",
                 ''
             );
         };
-        next();
+
+        // check if the token is valid
+        jwt.verify(
+            token,
+            config.jwt_access_secret as string,
+            function (err, decoded) {
+                if (err) {
+                    throw new AppError(
+                        StatusCodes.UNAUTHORIZED,
+                        "Unauthorized",
+                        ''
+                    )
+                };
+
+                // const { userId, role } = decoded;
+                req.user = decoded as JwtPayload;
+                next()
+            }
+        );
+
+        // next();
     })
 };
 export default authValidation;
