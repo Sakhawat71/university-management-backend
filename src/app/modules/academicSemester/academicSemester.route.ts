5b4ed0928bbbeb918bbbf97c43e901fb4d7b931f
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-29 01:02:56 +0600

    Add authentication middleware for creating academic semesters

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { academicSemesterController } from "./academicSemester.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { AcademicSemesterValidation } from "./academicSemester.validation";
+import authValidation from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constant";
 
 const router = Router();
 
 router.post(
     '/create-academic-semester',
+    authValidation(USER_ROLE.superAdmin, USER_ROLE.admin),
     validateRequest(AcademicSemesterValidation.createAcademicSemesterValidation),
     academicSemesterController.createAcademicSemester
 );
