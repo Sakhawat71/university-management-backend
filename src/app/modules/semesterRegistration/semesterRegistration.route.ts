90e388faced8e5cf7b375ec484332b8ba559bd83
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-30 14:38:25 +0600

    Enhance semester registration routes with role-based authentication for improved access control

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,27 +2,49 @@ import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { SemesterRegistrationValidation } from "./semesterRegistration.validation";
 import { semesterRegistrationController } from "./semesterRegistration.controller";
+import authValidation from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constant";
 
 const route = Router();
 
 route.post(
     '/create-semester-registration',
+    authValidation(
+        USER_ROLE.superAdmin,
+        USER_ROLE.admin
+    ),
     validateRequest(SemesterRegistrationValidation.createSemesterRegistrationSchemaValidation),
     semesterRegistrationController.createSemesterRegistration
 );
 
 route.get(
     '/',
+    authValidation(
+        USER_ROLE.superAdmin,
+        USER_ROLE.admin,
+        USER_ROLE.faculty,
+        USER_ROLE.student,
+    ),
     semesterRegistrationController.getAllSemesterRegistration
 );
 
 route.get(
     "/:id",
+    authValidation(
+        USER_ROLE.superAdmin,
+        USER_ROLE.admin,
+        USER_ROLE.faculty,
+        USER_ROLE.student,
+    ),
     semesterRegistrationController.getSingelSemesterRegistration
 );
 
 route.patch(
     '/:id',
+    authValidation(
+        USER_ROLE.superAdmin,
+        USER_ROLE.admin
+    ),
     validateRequest(SemesterRegistrationValidation.updateSemesterRegistrationSchemaValidation),
     semesterRegistrationController.updateSemesterRegistration
 )
